29ec076a6edc72fbc050c32c2ce1be8842235797
naghell <naghybusiness@gmail.com>
2024-02-11 23:48:09 -0300

    Deploy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="bg-black flex min-h-screen h-[1900px] flex-col items-center">
+    <main className="bg-black flex min-h-screen flex-col items-center">
       <NavBar />
     </main>
   );
