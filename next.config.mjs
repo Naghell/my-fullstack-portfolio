0b882d0c05cb73b1f40a54caccdfaf0544c5f709
naghell <naghybusiness@gmail.com>
2024-02-12 17:58:16 -0300

    Env

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? './my-fullstack-portfolio' : '',
-    assetPrefix: isProd ? './my-fullstack-portfolio' : '',
+    basePath: isProd ? '/my-fullstack-portfolio' : '',
+    assetPrefix: isProd ? '/my-fullstack-portfolio' : '',
 };
 
 export default nextConfig;
