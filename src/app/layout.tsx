8a51962b1bd56ee1e3bcdb3ea13f0cde4c2ccbc9
naghell <naghybusiness@gmail.com>
2024-02-12 17:33:46 -0300

    Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Renzo Luis Battaglino | Desarrollador Full Stack",
   description: "Experto en programación de webs y servidores.",
+  keywords: 'NextJS, React, JavaScript, TypeScript, programador web, full stack, freelance, frontend, backend, Argentina',
+  authors: {name: 'Renzo Luis Battaglino'}
 };
+
+const clairtyCode = `
+(function (c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "l0s9ag4csm");`
 
 export default function RootLayout({
   children,
@@ -17,6 +27,9 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>{children}</body>
+      <Script id="ms-clarity" strategy="afterInteractive">
+        {clairtyCode}
+      </Script>
     </html>
   );
 }
