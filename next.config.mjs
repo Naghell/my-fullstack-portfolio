c6832744e42867fac7a1923dc8b148a8a95146c0
naghell <naghybusiness@gmail.com>
2024-02-12 16:11:45 -0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // basePath: '/my-fullstack-portfolio/',
-    // assetPrefix: '/my-fullstack-portfolio/'
 };
 
 export default nextConfig;
